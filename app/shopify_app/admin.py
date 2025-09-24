bffe0361809998edcbaa4447c63d44ef5f1ab6f7
Ellie Cassady <cassadyeleanor@gmail.com>
2025-09-24 14:34:03 -0700

    updates

--- app/shopify_app/admin.py
+++ app/shopify_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from ywm_auth.models import User
 from home.models import SiteNav
+from ywm_auth.models import UserPermissions
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(SiteNav)
+
