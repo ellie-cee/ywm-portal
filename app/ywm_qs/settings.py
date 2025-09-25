9ee12cb2fed357a2d29eb7c97606ed280b3c48b6
Ellie Cassady <cassadyeleanor@gmail.com>
2025-09-25 16:55:59 -0700

    gitignore janitoring

--- app/ywm_qs/settings.py
+++ app/ywm_qs/settings.py
@@ -139,6 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    BASE_DIR / "staticfiles",
     "/var/www/static/",
 ]
 
