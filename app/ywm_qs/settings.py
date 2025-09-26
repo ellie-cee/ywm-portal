802bbcde85e92d52c0b89382c34e64a51accfc7f
Ellie Cassady <cassadyeleanor@gmail.com>
2025-09-25 17:16:05 -0700

    updates

--- app/ywm_qs/settings.py
+++ app/ywm_qs/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     #BASE_DIR / "static"
     #BASE_DIR / "staticfiles",
