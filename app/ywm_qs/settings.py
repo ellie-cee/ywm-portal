03b6138ac30146b8125c174cd0ac64ce9c0a2741
Ellie Cassady <cassadyeleanor@gmail.com>
2025-09-29 12:00:05 -0700

    hey now

--- app/ywm_qs/settings.py
+++ app/ywm_qs/settings.py
@@ -150,12 +150,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APPEND_SLASH = False
 
-EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
+
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
+#EMAIL_BACKEND = "anymail.backends.mailgun.EmailBackend"
 #EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #M_EMAIL = os.environ.get("DEFAULT_EMAIL")  # if you don't already have this in settings
 
 
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
 EMAIL_PORT = 25
 EMAIL_HOST_USER = os.environ.get("DEFAULT_EMAIL")
 #EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
